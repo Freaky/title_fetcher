a7efb87af0daa2e8ea8cf0b984d052a5fd0141e1
Thomas Hurst <tom@hur.st>
2015-08-13 18:47:25 +0100

    Timeouts and stuff

--- lib/element_grabber.rb
+++ lib/element_grabber.rb
@@ -12,15 +12,20 @@ class ElementGrabber
 	DEFAULT_READ_LIMIT    = 128 * 1024 # 128k
 	BLOCK_SIZE            = 32 * 1024
 	REDIRECTION_LIMIT     = 6
-	ALLOWED_SCHEMES       = %w(http https).freeze
 	ALLOWED_CONTENT_TYPES = %r{\A(?:text/|application/(?:xml|xhtml))}
+	DEFAULT_TIMEOUTS      = { read: 10, write: 10, connect: 10 }
 	DEFAULT_HEADERS       = {
 		HTTP::Headers::USER_AGENT => self.name
 	}
 
-	def initialize(tag, read_limit: DEFAULT_READ_LIMIT)
-		@tag = tag
+	def initialize(tag, read_limit: DEFAULT_READ_LIMIT, timeout: {}, headers: {})
+		@tag        = tag
+		@timeout    = timeout.merge(DEFAULT_TIMEOUTS)
 		@read_limit = Integer(read_limit)
+
+		@headers    = headers.merge(DEFAULT_HEADERS)
+		@http_client = HTTP.timeout(@timeout).headers(@headers).accept(:html)
+
 		@encoding_detector  = CharlockHolmes::EncodingDetector.new rescue CharlockHolmes::EncodingDetector
 		@encoding_converter = CharlockHolmes::Converter
 		@encoding_fixer     = MojiBake::Mapper.new
@@ -28,7 +33,6 @@ class ElementGrabber
 
 	def from_url(url)
 		content = nil
-		return unless ALLOWED_SCHEMES.include? URI.parse(url).scheme
 
 		return unless response = get_with_redirect(url)
 		return unless ALLOWED_CONTENT_TYPES.match response.content_type.mime_type
@@ -41,6 +45,8 @@ class ElementGrabber
 		so_far = 0
 		reader = Enumerator.new do |parser|
 			while chunk = response.body.readpartial(BLOCK_SIZE)
+				next if chunk.empty?
+
 				so_far += chunk.size
 				detected_encoding ||= guess_encoding(chunk)
 				parser << convert_encoding(chunk, detected_encoding)
@@ -69,26 +75,25 @@ class ElementGrabber
 		@encoding_fixer.recover(converted).scrub
 	end
 
+	STATUS_REDIRECT = [301, 302, 303, 307, 308].freeze # *glares at http.rb*
+
 	def get_with_redirect(url, redirections = Set.new)
-		response = HTTP.headers(DEFAULT_HEADERS).get(url)
+		response = get(url)
+		return response if response.status.ok?
 
-		case response.status.code
-		when 301, 302, 303, 307, 308
-			target = response['Location']
-			return nil if target.nil? or target.empty?
-
+		if STATUS_REDIRECT.include? response.status.code and target = response['Location']
 			if target.start_with?('/')
 				uri = URI.parse(url)
 				target = "#{uri.scheme}://#{uri.host}:#{uri.port}#{target}"
 			end
 
-			return if redirections.include?(target)
-			return if redirections.size > REDIRECTION_LIMIT
-			redirections << target
-
+			return unless redirections.add?(target) and redirections.size < REDIRECTION_LIMIT
 			return get_with_redirect(target, redirections)
-		else return response if response.status.ok?
 		end
+	end
+
+	def get(url)
+		@http_client.get(url)
 	end
 
 	class ElementExtractor
